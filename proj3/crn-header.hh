7a95df517471689aa2d0786321e553d32125a3d4
comnetsii <sugang.li718@gmail.com>
2015-05-05 11:48:25 -0400

    packet format changed

--- proj3/crn-header.hh
+++ proj3/crn-header.hh
@@ -6,9 +6,10 @@ CLICK_DECLS
 struct CrnPacket{
 	
 	uint8_t type;//0 for request, 1 for content and 2 for update 
-	uint32_t hopcount;
-	uint32_t content_id;
-	in_addr my_interface;
+	uint8_t hopcount;
+	uint8_t content_id;
+	in_addr in_interface;
+	in_addr out_interface;
 	const unsigned char* data;
 	
 };
